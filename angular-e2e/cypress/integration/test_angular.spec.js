9373ea9315b36e77b8fc182217bf1064764d41a1
bory-kb <s.kubo0527@gmail.com>
2022-03-28 15:16:04 +0900

    test_init

---
+++ angular-e2e/cypress/integration/test_angular.spec.js
@@ -0,0 +1,30 @@
+// test_angular.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
+      cy.visit('http://localhost:4200/#/')
+    })
+  
+    it('is running のところ', () => {
+      // We use the `cy.get()` command to get all elements that match the selector.
+      // Then, we use `should` to assert that there are two matched items,
+      // which are the two default items.
+      cy.get('body > app-root > div.content > div.card.highlight-card.card-small > span').should('have.text', 'angular-e2e app is running!')
+      cy.get('body > app-root > div.content > div.terminal > pre').should('have.text', 'ng generate component xyz')
+      // We can go even further and check that the default todos each contain
+      // the correct text. We use the `first` and `last` functions
+      // to get just the first and last matched elements individually,
+      // and then perform an assertion with `should`.
+
+    })
+})
